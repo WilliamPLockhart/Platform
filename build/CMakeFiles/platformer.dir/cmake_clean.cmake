5bd1263d78d54e7c97acfa8192c683d3a3db7adf
Will Lockhart <willlockhart65@gmail.com>
2024-10-23 01:02:34 -0600

    created the basic code structure

---
+++ build/CMakeFiles/platformer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/platformer.dir/game.cpp.o"
+  "CMakeFiles/platformer.dir/game.cpp.o.d"
+  "CMakeFiles/platformer.dir/main.cpp.o"
+  "CMakeFiles/platformer.dir/main.cpp.o.d"
+  "platformer"
+  "platformer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/platformer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
